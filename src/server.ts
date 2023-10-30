7445cff9ac16d95fb67f929dee64b5f28b8d41be
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2023-10-29 21:31:11 -0300

    Update server.ts
    Change port from 3333 to 3000

--- src/server.ts
+++ src/server.ts
@@ -81,7 +81,7 @@ app.delete('/contacts/:id', async (request) => {
 
 app.listen({
   host: '0.0.0.0',
-  port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  port: process.env.PORT ? Number(process.env.PORT) : 3000,
 }).then(() => {
   console.log('HTTP server running')
 })
